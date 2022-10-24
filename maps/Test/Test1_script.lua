a7fcd1e07a0f54ac29e32e792a838f15973ddbe6
4z0t <gastec139@gmail.com>
2022-10-24 14:25:32 +0300

    wip

--- maps/Test/Test1_script.lua
+++ maps/Test/Test1_script.lua
@@ -10,11 +10,45 @@ local Cinematics = import('/lua/cinematics.lua')
 local Buff = import('/lua/sim/Buff.lua')
 local TauntManager = import('/lua/TauntManager.lua')
 
+local objectiveBuilder = import("/lua/ASF/ObjectiveBuilder.lua").ObjectiveBuilder()
+local VOStrings = import("/maps/Test/VOStrings.lua").lines
+
+local objectives = ObjectiveManager():Init
+{
+	objectiveBuilder
+		:New "start"
+		:Title "TEST"
+		:Description "Test"
+		:To "timer"
+		:Target
+		{
+			ShowProgress = true,
+			Timer = 60,
+			ExpireResult = 'complete',
+		}
+		:OnStart(function()
+			Cinematics.NISMode(
+				function()
+					Cinematics.CameraMoveToMarker(ScenarioUtils.GetMarker("Cam1"), 0)
+					ScenarioFramework.Dialogue(VOStrings.Start, nil, true)
+					WaitSeconds(1)
+					Cinematics.CameraMoveToMarker(ScenarioUtils.GetMarker("Cam2"), 3)
+					WaitSeconds(1)
+					Cinematics.CameraMoveToMarker(ScenarioUtils.GetMarker("Cam3"), 3)
+				end
+			)
+		end)
+		:OnSuccess(function()
+
+		end)
+		:Create()
+}
+
 function OnPopulate()
-	ScenarioUtils.InitializeArmies()
+	ScenarioUtils.InitializeScenarioArmies()
 end
 
 function OnStart(self)
 	ScenarioFramework.SetPlayableArea('StartArea', false)
-
+	objectives:Start("start")
 end
---
+++ maps/Test/VOStrings.lua
@@ -0,0 +1,7 @@
+lines = {
+
+    Start = {
+        { text = '[TheWheelie]: GG WP.',
+            vid = '', bank = 'JJ_VO2', cue = 'Intro1_1', faction = 'UEF' },
+    }
+}
