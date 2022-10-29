be38a8a0580be51812ef31fdfe96930ce502b638
4z0t <gastec139@gmail.com>
2022-10-30 01:09:08 +0300

    add OpAi

--- maps/Test/YudiOpAI.lua
+++ maps/Test/YudiOpAI.lua
@@ -1,4 +1,5 @@
 local PlatoonBuilder = import("/lua/ASF/PlatoonBuilder.lua")
+local OpAIBuilder = import("/lua/ASF/OpAIBuilder.lua").OpAIBuilder
 local PlatoonLoader = import("/lua/ASF/PlatoonLoader.lua").PlatoonLoader
 local UNIT = import("/lua/ASF/UnitNames.lua").Get
 local BaseManager = import('/lua/ai/opai/basemanager.lua')
@@ -18,12 +19,17 @@ function Main()
     local pb = PlatoonBuilder.Create()
         :UseAIFunction(SPAIFileName, "PatrolChainPickerThread")
         :UseType('Land')
-
+    ---@type OpAIBuilder
+    local opAIb = OpAIBuilder()
 
     DifficultyValue.Extend {
         ["Brick count"] = { 3, 4, 5 },
         ["Banger count"] = { 1, 2, 3 },
-        ["Deceiver count"] = { 0, 0, 1 }
+        ["Deceiver count"] = { 0, 0, 1 },
+
+        ["M Brick count"] = { 6, 8, 10 },
+        ["M Banger count"] = { 3, 4, 5 },
+        ["M Deceiver count"] = { 0, 1, 2 }
     }
 
     ---@type PlatoonLoader
@@ -31,7 +37,7 @@ function Main()
     pl:LoadPlatoons {
         pb:Default "Brick Attack"
             :InstanceCount(10)
-            :Priority(100)
+            :Priority(200)
             :AddGroupDefault(UNIT "Brick", DV "Brick count")
             :AddGroupDefault(UNIT "Banger", DV "Banger count")
             :AddGroupDefault(UNIT "Deceiver", DV "Deceiver count")
@@ -43,9 +49,63 @@ function Main()
                     "LAC03",
                 }
             }
-            :Create()
+            :Create(),
+        pb:Default "Lone Brick"
+            :InstanceCount(5)
+            :Priority(100)
+            :AddGroupDefault(UNIT "Brick", 1)
+            :AddGroupDefault(UNIT "Deceiver", DV "Deceiver count")
+            :Data
+            {
+                PatrolChains = {
+                    "LAC01",
+                    "LAC02",
+                    "LAC03",
+                }
+            }
+            :Create(),
+        pb:Default "Massive Brick Attack"
+            :InstanceCount(10)
+            :Priority(100)
+            :AddGroupDefault(UNIT "Brick", DV "M Brick count")
+            :AddGroupDefault(UNIT "Banger", DV "M Banger count")
+            :AddGroupDefault(UNIT "Deceiver", DV "M Deceiver count")
+            :Data
+            {
+                PatrolChains = {
+                    "LAC01",
+                    "LAC02",
+                    "LAC03",
+                }
+            }
+            :Create(),
 
     }
+
+    pl:LoadOpAIs
+    {
+
+        opAIb
+            :New "Engi attack"
+            :Type "EngineerAttack"
+            :Quantity("T1Engineer", 4)
+            :Data
+            {
+                MasterPlatoonFunction = { SPAIFileName, 'SplitPatrolThread' },
+                PlatoonData = {
+                    PatrolChains = {
+                        "LAC01",
+                        "LAC02",
+                        "LAC03",
+                    },
+                },
+                Priority = 300,
+            }
+            :Create(),
+
+
+    }
+
 
 
 end
