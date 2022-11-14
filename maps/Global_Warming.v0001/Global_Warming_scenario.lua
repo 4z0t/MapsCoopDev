c3922c3374be38de24300e29ccf4107de1ca2a15
4z0t <gastec139@gmail.com>
2022-11-14 13:53:41 +0300

    GW

---
+++ maps/Global_Warming.v0001/Global_Warming_scenario.lua
@@ -0,0 +1,27 @@
+version = 3 -- Lua Version. Dont touch this
+ScenarioInfo = {
+    name = "Global Warming",
+    description = "",
+    preview = '',
+    map_version = 1,
+    type = 'skirmish',
+    starts = true,
+    size = {1024, 1024},
+    reclaim = {0, 0},
+    map = '/maps/Global_Warming.v0001/Global_Warming.scmap',
+    save = '/maps/Global_Warming.v0001/Global_Warming_save.lua',
+    script = '/maps/Global_Warming.v0001/Global_Warming_script.lua',
+    norushradius = 40,
+    Configurations = {
+        ['standard'] = {
+            teams = {
+                {
+                    name = 'FFA',
+                    armies = {'ARMY_1', 'ARMY_2', 'ARMY_3', 'ARMY_4', 'ARMY_5', 'Yudi'}
+                },
+            },
+            customprops = {
+            },
+        },
+    },
+}
